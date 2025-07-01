218816ac067e31f677b08ff7e694e219bcc95b4c
rajchuahn-12 <rajchuahan25112004@gmail.com>
2025-07-01 16:10:23 +0530

    update

--- mongodb/q9.js
+++ mongodb/q9.js
@@ -1,4 +1,4 @@
-//      Create a folder mongo-replica and sub folders data1 data2 and data3
+//Create a folder mongo-replica and sub folders data1 data2 and data3
 
 // Open command prompt and start running servers on separate tabs
 
@@ -12,19 +12,18 @@
 
 // mongosh --port 27018
 
-// rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
+//rs.initiate({_id:"rs1",members:[{_id:0,host:"127.0.0.1:27018"},{_id:1,host:"127.0.0.1:27019"},{_id:2,host:"127.0.0.1:27020"}]})
 
-// rs.config()  //to check the config
-// rs.status()
+//rs.config()  //to check the config
+//rs.status()
 
-// Use mongosh command with the following connection string and the primary server will automatically get connected:
+//Use mongosh command with the following connection string and the primary server will automatically get connected:
 
+//mongosh "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=rs1"
 
-// mongosh "mongodb://localhost:27018,localhost:27019,localhost:27020/?replicaSet=rs1"
-
-// show dbs
-// use mytestdb
-// db.createCollection("customers")
+//show dbs
+//use mytestdb
+//db.createCollection("customers")
 // db.customers.insertOne({name:"John"})
 
 // Check secondary servers. Check both the servers if data is replicated
@@ -39,3 +38,8 @@
 // db.getMongo().setReadPref("secondary")  //or rs.secondaryOk()
 // use mytestdb
 // db.customers.find() – will work now
+
+
+//rs,initiate({_id:'cf,members:[{_id:0, host:'localhost:27018'},{_id:1,host:'localhost'}])
+// rs.config()
+// rs.status()
